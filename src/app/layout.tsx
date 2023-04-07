3679d43f4d8d27eba8f0790f920c710fead70197
almeidx <almeidx@pm.me>
2023-04-07 19:26:47 +0100

    use file-based favicon/robots.txt

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,13 +9,9 @@ const roboto = Roboto({ subsets: ["latin"], weight: ["300", "400", "500"] });
 export default function RootLayout({ children }: { children: ReactNode }) {
 	return (
 		<html className={roboto.className} lang="en">
-			<head>
-				<link href="/favicon.ico" rel="shortcut icon" />
-			</head>
-
 			<body>
 				{/* The extra div is needed due to next.js' next-route-announcer, which gets appended to the body */}
-				<div className="grid h-screen min-h-screen place-items-center bg-gradient-to-b from-black to-gray-800 text-white">
+				<div className="grid min-h-screen place-items-center bg-gradient-to-b from-black to-gray-800 text-white">
 					{children}
 				</div>
 			</body>
