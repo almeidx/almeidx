e834391c0cecd33c7da5b86e0456c7dd02701b9f
almeidx <github@almeidx.dev>
2024-06-03 15:34:56 +0100

    fix node version, unfold src

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,7 @@ import type { Config } from "tailwindcss";
 import { fontFamily } from "tailwindcss/defaultTheme";
 
 export default {
-	content: ["./src/**/*.{tsx,css}"],
+	content: ["./app/**/*.{tsx,css}"],
 	theme: {
 		extend: {
 			fontFamily: {
