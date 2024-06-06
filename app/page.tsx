5f6b6c9b53de970235f3be75fe639f3275d1f1aa
almeidx <github@almeidx.dev>
2024-06-06 22:52:55 +0100

    upgrade to tailwindcss v4

--- app/page.tsx
+++ app/page.tsx
@@ -31,7 +31,7 @@ export default function Page() {
 		<div className="max-w-2xl flex-col items-center justify-center px-4">
 			<header>
 				<h1 className="text-4xl font-medium hover:animate-pulse">Almeida</h1>
-				<p className="text-xl font-light text-gray-400">College student and hobbyist developer</p>
+				<p className="text-xl font-light text-gray-400">Computer Science undergradute</p>
 			</header>
 
 			<div className="mt-4 flex gap-3 text-lg text-gray-300">
