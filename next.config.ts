b6adaa096f10f71cbe62d82fd45c3988c1325121
almeidx <github@almeidx.dev>
2025-10-23 22:26:51 +0100

    next 16

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 export default {
+	cacheComponents: true,
 	experimental: {
+		turbopackFileSystemCacheForDev: true,
 		viewTransition: true,
 	},
 	reactCompiler: true,
