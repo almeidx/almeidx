57a7336b14e1ce9fea53698c0d48d86ce5559359
almeidx <github@almeidx.dev>
2026-06-23 14:16:27 +0100

    chore: use Tailwind Vite plugin

--- astro.config.ts
+++ astro.config.ts
@@ -1,8 +1,12 @@
 import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
 	build: {
 		format: "file",
 	},
 	trailingSlash: "never",
+	vite: {
+		plugins: [tailwindcss()],
+	},
 });
